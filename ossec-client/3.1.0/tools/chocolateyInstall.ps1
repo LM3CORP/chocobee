61ea5845d5d041f4f69f3b32de436d3ce1d1154f
lmayorga <lmayorga@lm3corp.com>
2018-10-19 10:13:57 -0400

    fix choco package 3.1.0

--- ossec-client/3.1.0/tools/chocolateyInstall.ps1
+++ ossec-client/3.1.0/tools/chocolateyInstall.ps1
@@ -5,6 +5,9 @@ $packageArgs = @{
   url            = 'https://updates.atomicorp.com/channels/atomic/windows/ossec-agent-win32-3.1.0-5696.exe'
   installerType  = 'exe'
   checksum       = '5D84C7F6B75B9E9AB5FFE53553298DF08186D0819611892AB8219349388D4C02'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
